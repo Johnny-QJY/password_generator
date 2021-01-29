71d5624b6cfe4d0db035db531fb0d789c6179081
Johnny <johnny860703@gmail.com>
2021-01-29 13:53:00 +0800

    fest:complete

---
+++ generate_password.js
@@ -0,0 +1,55 @@
+// generate_password.js 
+
+// define sample function to randomly return a item in an array
+function sample(array) {
+  const index = Math.floor(Math.random() * array.length)
+  return array[index]
+}
+
+// define generatePassword function
+function generatePassword(options) {
+  // define things user might want
+  const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
+  const upperCaseLetters = lowerCaseLetters.toUpperCase()
+  const numbers = '1234567890'
+  const symbols = '`~!@$%^&*()-_+={}[]|;:"<>,.?/'
+
+  // creat a collection to store things user picked up
+  let collection = []
+
+  if (options.lowercase === 'on') {
+    collection = collection.concat(lowerCaseLetters.split(''))
+  }
+  if (options.uppercase === 'on') {
+    collection = collection.concat(upperCaseLetters.split(''))
+  }
+  if (options.numbers === 'on') {
+    collection = collection.concat(numbers.split(''))
+  }
+  if (options.symbols === 'on') {
+    collection = collection.concat(symbols.split(''))
+  }
+
+  // remove things user do not need 
+  if (options.excludeCharacters) {
+    console.log(`exclude characters: ${options.excludeCharacters}`)
+    collection = collection.filter(
+      character => !options.excludeCharacters.includes(character)
+    )
+  }
+  // return error notice if collection is empty
+  if (collection.length === 0) {
+    return 'There is no valid characters in your selection.'
+  }
+
+  // start generating password
+  let password = ''
+  for (let i = 0; i < options.length; i++) {
+    password += sample(collection)
+  }
+  // return the generated password
+  return password
+}
+
+// invoke generatePass function
+module.exports = generatePassword
